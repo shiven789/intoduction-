b5b8029f9a9366cc4c08f7a061fbde2088435eb3
shiven789 <shiven789@icloud.com>
2025-01-25 12:07:54 +0000

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,21 @@
+import pgzrun
+WIDTH=300
+HEIGHT=300
+
+def draw():
+
+
+    screen.fill("red")
+    r= Rect((100,100),(150,60))
+    #screen.draw.rect(r,"green")
+    screen.draw.filled_rect(r,"green")
+    
+
+pgzrun.go()
+
+
+
+
+
+
+
